e309f02ba5cef65318f7ef6bfd2246dcfbbcfe11
Saïd Chikh <said.chikh@ores.net>
2016-04-27 12:48:20 +0200

    first commit

---
+++ wwwroot/app/config.route.js
@@ -0,0 +1,101 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app');
+
+    //app.constant('routes', getRoutes());
+
+    app.config(['$stateProvider', '$urlRouterProvider', 
+        function ($stateProvider, $urlRouterProvider) {
+            $stateProvider.state('home', {
+                    url: '/',
+                    views: {
+                        '@': { templateUrl: 'app/welcomeView.html' }
+                    }
+                })
+                
+                .state('ol1', {
+                    abstract: true,
+                    url: '/ol1',
+                    views: {
+                        '@': { 
+                            templateUrl: 'app/ol1/ol1frame.html', 
+                            controller: 'toolbarController as vm'
+                        }
+                    }
+                })                
+                .state('ol1.x', {
+                    url: '/x',
+                    views: {
+                        'map': {
+                            templateUrl: 'app/ol1/ol11EditView.html',
+                            controller: 'ol11EditCtrl as vm'
+                        }
+                    }
+                })
+                
+                .state('ol1.2', {
+                    url: '/ol1.2',
+                    views: {
+                        '@': {
+                            templateUrl: 'app/ol1/ol12EditView.html',
+                            controller: 'ol12EditCtrl as vm'
+                        }
+                    }
+                })
+                // .state('ngeoSchema', {
+                //     url: '/ngeoSchema',
+                //     views: {
+                //         '@': {
+                //             templateUrl: 'app/ngeoSchema/ngeoSchemaEditView.html',
+                //             controller: 'ngeoSchemaEditCtrl as vm'
+                //         }
+                //     }
+                // })
+                .state('basicSchema', {
+                    url: '/basicSchema',
+                    views: {
+                        '@': {
+                            templateUrl: 'app/basicSchema/basicSchemaEditView.html',
+                            controller: 'basicSchemaEditCtrl as vm'
+                        }
+                    }
+                })
+                .state('customerList', {
+                    url: '/customers',
+                    views: {
+                        '@': {
+                            templateUrl: 'app/customers/customerListView.html',
+                            controller: 'customerListCtrl as vm'
+                        },
+                        'side-nav@': { templateUrl: 'app/customers/customerListSideBar.html' }
+                    }
+                })
+                .state('customerList.customerEdit', {
+                    url: '/edit/:customerId',
+                    views: {
+                        '@': {
+                            templateUrl: 'app/customers/customerEditView.html',
+                            controller: 'customerEditCtrl as vm',
+                            resolve: {
+                                customerResource: 'customerResource',
+                                customer: function (customerResource, $stateParams) {
+                                    var customerId = $stateParams.customerId;
+                                    return customerResource.get({ customerId: customerId }).$promise;
+                                }
+                            }
+                        },
+                        'side-nav@': { templateUrl: 'app/customers/customerListSideBar.html' }
+                    }
+                });
+            $urlRouterProvider.otherwise('/');
+        }
+    ]);
+
+    app.run(['$rootScope', '$state', '$stateParams',
+        function ($rootScope, $state, $stateParams) {
+            $rootScope.$state = $state;
+            $rootScope.$stateParams = $stateParams;
+        }
+    ]);
+})();
